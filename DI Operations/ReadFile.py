c59d0da75e28c8077a5273cbd2662553269f505e
kandpalpranav <55237329+kandpalpranav@users.noreply.github.com>
2021-02-21 14:43:41 +0100

    Create ReadFile.py

---
+++ DI Operations/ReadFile.py
@@ -0,0 +1,15 @@
+import  pandas as pd
+from io import StringIO
+from hdfs import InsecureClient , HdfsError
+client = InsecureClient('http://datalake:50070')
+
+def read_file(msg):
+    
+    attr = dict()
+    api.send("outData",api.Message(attributes=attr, body= 'Param Loading Triggered'))
+    sdl_file = '/shared/SLT/SFLIGHT/data.csv'
+    with client.read(sdl_file , encoding='utf-8') as reader:
+        df_key = pd.read_csv(reader)
+        api.send("outData",api.Message(attributes=attr, body= str(df_key)))
+
+api.set_port_callback("input", read_file)
