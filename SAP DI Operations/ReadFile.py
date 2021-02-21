c565f4bf55174a5ae3d8fd7a98b4918444773dc0
kandpalpranav <55237329+kandpalpranav@users.noreply.github.com>
2021-02-21 17:26:45 +0100

    Update ReadFile.py

--- SAP DI Operations/ReadFile.py
+++ SAP DI Operations/ReadFile.py
@@ -9,7 +9,7 @@ def read_file(msg):
     api.send("outData",api.Message(attributes=attr, body= 'Param Loading Triggered'))
     sdl_file = '/shared/SLT/SFLIGHT/data.csv'
     with client.read(sdl_file , encoding='utf-8') as reader:
-        df_key = pd.read_csv(reader)
+        df = pd.read_csv(reader)
         api.send("outData",api.Message(attributes=attr, body= str(df_key)))
         api.send("out2",str(df_key))
 
